d611d47ef17dc2ac5af9a60111b7b4aab0e0db97
Dan Levy <397632+justsml@users.noreply.github.com>
2018-04-06 14:14:53 -0600

    Rearrange express template app.js
    Simplified imports & organization

--- express/app.js
+++ express/app.js
@@ -1,35 +1,33 @@
-// TODO: INSTALL PRE-REQS: `npm install express cors body-parser morgan monk`
-
-const http        = require('http')
+// TODO: INSTALL PRE-REQS: 
+//  npm install express cors body-parser morgan nodemon
+// Optional Extras: 
+//  npm install monk knex 
 const express     = require('express')
 const bodyParser  = require('body-parser')
 const morgan      = require('morgan')
 const cors        = require('cors')
 const app         = module.exports = express()
-const server      = http.createServer(app)
 const port        = parseInt(process.env.PORT || 3000)
-const devMode     = process.env.NODE_ENV !== 'production'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan(devMode ? 'dev' : 'combined'))
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
 app.use(cors({origin: true}))
+// Optional Static file handler:
+// app.use('/', express.static('./build'))
 
-// TODO: ADD (MOUNT) YOUR MIDDLEWARE (ROUTES) HERE
-// ^^^ Example: app.use('/v1/kitten', require('./routes/kitten'))
-// ^^^ Example: app.use('/cats', require('./routes/kitten'))
+// TODO: ADD (MOUNT) YOUR MIDDLEWARE (ROUTES) HERE:
+// Example: app.use('/api/kitten', require('./routes/kitten'))
 
+
+// These 2 `app.use` MUST be last `.use`'s
 app.use(notFound)
 app.use(errorHandler)
-
-server.listen(port)
-  .on('error',     console.error.bind(console))
-  .on('listening', console.log.bind(console, 'Listening on ' + port));
 
 function notFound(req, res, next) {
   const url = req.originalUrl
   if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
-    // Don't log less important auto requests
+    // Don't log less important (automatic) browser requests
     console.error('[404: Requested file not found] ', url)
   }
   res.status(404).send({error: 'Url not found', status: 404, url})
@@ -37,6 +35,10 @@ function notFound(req, res, next) {
 
 function errorHandler(err, req, res, next) {
   console.error('ERROR', err)
-  const stack =  devMode ? err.stack : undefined
+  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
   res.status(500).send({error: err.message, stack, url: req.originalUrl})
 }
+
+app.listen(port)
+  .on('error',     console.error.bind(console))
+  .on('listening', console.log.bind(console, 'Listening on ' + port));
