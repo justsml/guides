3b4cd947fdf725e21dbc2ff62db46ac30a818e96
Dan Levy <397632+justsml@users.noreply.github.com>
2018-10-02 02:36:06 -0600

    Cleaned up 404 handler, instructions
    App.js now under 40 lines.

--- express/setup-guide/app.js
+++ express/setup-guide/app.js
@@ -1,4 +1,4 @@
-// From Dan's Guides: https://github.com/justsml/guides/tree/master/express
+// From Dan's Guides: https://github.com/justsml/guides/tree/master/express/setup-guide
 // TODO: INSTALL PRE-REQS:
 //  npm install express cors body-parser morgan nodemon
 const express     = require('express')
@@ -11,26 +11,19 @@ const port        = parseInt(process.env.PORT || 3000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
-app.use(cors({origin: true, credentials: true}))
-// Optional Static file handler:
+app.use(cors({origin: true, credentials: true})) // <= Disable if you don't need CORS
+// TODO: Optional Static file handler:
 // app.use('/', express.static('./build'))
 
 // TODO: ADD (MOUNT) YOUR MIDDLEWARE (ROUTES) HERE:
 // Example: app.use('/api/cat', require('./routes/cat'))
 
-
-// These 2 `app.use` MUST be last `.use`'s
+// The following 2 `app.use`'s MUST follow ALL your routes/middleware
 app.use(notFound)
 app.use(errorHandler)
 
-// eslint-disable-next-line
 function notFound(req, res, next) {
-  const url = req.originalUrl
-  if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
-    // Don't log less important (automatic) browser requests
-    console.error('[404: Requested file not found] ', url)
-  }
-  res.status(404).send({error: 'Url not found', status: 404, url})
+  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
 }
 
 // eslint-disable-next-line
@@ -42,4 +35,4 @@ function errorHandler(err, req, res, next) {
 
 app.listen(port)
   .on('error',     console.error.bind(console))
-  .on('listening', console.log.bind(console, 'Listening on ' + port))
+  .on('listening', console.log.bind(console, 'Listening on http://0.0.0.0:' + port))
